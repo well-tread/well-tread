3992fe75c3bf2cba1f81fb157e94cbcf94ea9391
Blake Buck <blakemanbuck@gmail.com>
2019-03-15 20:09:36 -0500

    First Commit

---
+++ src/Components/Search/Search.tsx
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+
+export interface Props{
+
+}
+
+export interface State{
+    
+}
+
+class Search extends Component<Props, State>{
+    constructor(props:Props){
+        super(props);
+        this.state={
+            
+        }
+    }
+    
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Search;
---
+++ src/Components/TrailPage/Reviews/Comment/Comment.tsx
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+
+export interface Props{
+
+}
+
+export interface State{
+    
+}
+
+class Comment extends Component<Props, State>{
+    constructor(props:Props){
+        super(props);
+        this.state={
+            
+        }
+    }
+    
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Comment;
