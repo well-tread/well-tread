e969871ee13315e5e246472c7ae4a06144af8792
Blake Buck <blakemanbuck@gmail.com>
2019-03-18 15:34:49 -0500

    Put router in index.tsx, did work on Account

--- src/Components/Account/Account.tsx
+++ src/Components/Account/Account.tsx
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import Preferences from './Preferences/Preferences'; 
 
+import firebase from '../../firebase';
+
 //materialUI imports
 import { withStyles, createStyles, Theme , createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
@@ -9,6 +11,7 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import SettingsIcon from '@material-ui/icons/Settings';
@@ -25,18 +28,36 @@ const theme = createMuiTheme({
 })
 
 const styles = (theme:Theme) =>createStyles({
+    paper:{
+        backgroundColor:'#FCFCFC'
+    },
     expansionPanels:{
         
     },
+    expansionPanel:{
+        backgroundColor:'#F7F7F7',
+    },
     userBar:{
-
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        marginTop:'15%'
+    },
+    avatar:{
+        width:100,
+        height:100,
+        fontSize:'2em',
+        backgroundColor:'#FF5722',
     }
 })
 
 export interface Props{
     classes:{
+        paper:string;
         expansionPanels:string;
+        expansionPanel:string;
         userBar:string;
+        avatar:string
     }
 }
 
@@ -63,20 +84,22 @@ class Account extends Component<Props, State>{
         const {preferencesIsOpen} = this.state;
         return(
             <MuiThemeProvider theme={theme}>
+            <Paper className={classes.paper}>
 
             <div className={classes.userBar}>
                 <IconButton onClick={()=>this.togglePreferences()}>
-                    <SettingsIcon />
+                    <Avatar className={classes.avatar} src='https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Foriginal%2F000%2F019%2F930%2F1421657233490.jpg&f=1'>T</Avatar>
                 </IconButton>
+                <Typography component='p'>Username goes here</Typography>
             </div>
 
             <Typography component='h2' variant='h2'>Map</Typography>
 
             <div className={classes.expansionPanels}>
 
-                <ExpansionPanel>
+                <ExpansionPanel className={classes.expansionPanel}>
 
-                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon color='secondary'/>}>
                         <Typography>Favorited Trails</Typography>
                     </ExpansionPanelSummary>
 
@@ -88,9 +111,9 @@ class Account extends Component<Props, State>{
 
                 </ExpansionPanel>
 
-                <ExpansionPanel>
+                <ExpansionPanel className={classes.expansionPanel}>
 
-                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon color='secondary'/>}>
                         <Typography>Completed Trails</Typography>
                     </ExpansionPanelSummary>
 
@@ -105,7 +128,9 @@ class Account extends Component<Props, State>{
                 <Preferences preferencesIsOpen={preferencesIsOpen} togglePreferences={this.togglePreferences}/>
 
             </div>
+            </Paper>
             </MuiThemeProvider>
+            
         )
     }
 }
