96e773ed03c68202109e285d8a3c95b335daa5f2
Kevin Edmondson <kevinraye13@gmail.com>
2019-03-19 09:13:50 -0500

    Single ID trails page

--- src/Components/TrailPage/TrailPage.tsx
+++ src/Components/TrailPage/TrailPage.tsx
@@ -3,19 +3,40 @@ import { withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import Whatshot from "@material-ui/icons/Whatshot";
+import Typography from "@material-ui/core/Typography";
 import "./trailpage.css";
 import axios from "axios";
-import { string } from "prop-types";
 
-const styles = {
+// const styles = {
+//   root: {
+//     flexGrow: 1
+//   }
+// };
+const styles = (theme: any) => ({
   root: {
-    flexGrow: 1
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    width: "70vw",
+    margin: "0 auto",
+    "margin-bottom": "2em",
+    "margin-top": "2em"
+  },
+  button: {
+    margin: theme.spacing.unit
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit
   }
-};
+});
 
 export interface Props {
   classes: {
     root: string;
+    button: string;
+    leftIcon: string;
   };
 }
 
@@ -52,13 +73,35 @@ class TrailPage extends Component<Props, State> {
         return (
           <div className="trailContainer" key={index}>
             <img className="trailImage" src={element.imgMedium} alt="" />
-            <div className="trailInfo">
-              <h1>{element.name}</h1>
-              <h2>Stars: {element.stars}</h2>
-              <h2>Difficulty: {element.difficulty}</h2>
-              <h2>{element.length} Miles</h2>
-              <h2>Description: {element.summary}</h2>
-            </div>
+            <Paper className={classes.root}>
+              <div className="trailInfo">
+                <Typography variant="h4">{element.name}</Typography>
+                <div className="sideBySide">
+                  <Typography variant="h5" id="miles">
+                    <strong>Miles: </strong>
+                    {element.length}
+                  </Typography>
+                  <Typography variant="h5" id="stars">
+                    <strong> Stars: </strong> {element.stars}
+                  </Typography>
+                </div>
+                <Typography variant="h5" id="info">
+                  <strong>Difficulty: </strong> {element.difficulty}
+                </Typography>
+
+                <Typography variant="h5" id="info">
+                  <strong>Description: </strong> {element.summary}
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="default"
+                  className={classes.button}
+                >
+                  <Whatshot className={classes.leftIcon} />
+                  Favorite
+                </Button>
+              </div>
+            </Paper>
           </div>
         );
       })
@@ -67,20 +110,20 @@ class TrailPage extends Component<Props, State> {
     );
     return (
       <div>
-        <Paper className={classes.root}>
-          <Tabs
-            value={this.state.value}
-            onChange={this.handleChange}
-            indicatorColor="primary"
-            textColor="primary"
-            centered
-            id="tabs"
-          >
-            <Tab label="Map" />
-            <Tab label="Trail" />
-            <Tab label="Reviews" />
-          </Tabs>
-        </Paper>
+        {/* <Paper className={classes.root}> */}
+        <Tabs
+          value={this.state.value}
+          onChange={this.handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+          id="tabs"
+        >
+          <Tab label="Map" />
+          <Tab label="Trail" />
+          <Tab label="Reviews" />
+        </Tabs>
+        {/* </Paper> */}
         {Trail0}
       </div>
     );
