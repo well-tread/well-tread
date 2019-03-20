4506737dcecf36e3428d2ed8b5af7132dd988992
Kevin Edmondson <kevinraye13@gmail.com>
2019-03-20 13:46:45 -0500

    homepage recieving favorites

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,28 +1,41 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export interface Props{
+export interface Props {}
 
+export interface State {
+  popularBikingTrails: any;
 }
 
-export interface State{
-    
-}
+class Home extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      popularBikingTrails: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(
+        `https://www.mtbproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=200&minStars=4&maxResults=5&key=200430946-fc66551e94fef44057cb0cc88316bbec`
+      )
+      .then(res => {
+        const response = res.data;
+        console.log(res.data.trails);
+        this.setState({
+          popularBikingTrails: response.trails
+        });
+      })
 
-class Home extends Component<Props, State>{
-    constructor(props:Props){
-        super(props);
-        this.state={
-            
-        }
-    }
-    
-    render(){
-        return(
-            <div>
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
-            </div>
-        )
-    }
+  render() {
+    console.log(this.state.popularBikingTrails);
+    return <div>Best Home Page</div>;
+  }
 }
 
 export default Home;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Login from "./Components/Login/Login";
-import Account from "./Components/Account/Account";
-import TrailPage from "./Components/TrailPage/TrailPage";
-import Search from "./Components/Search/Search";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Account from './Components/Account/Account';
+import TrailPage from './Components/TrailPage/TrailPage';
+import Search from './Components/Search/Search';
+import Home from './Components/Home/Home';
 
 export default (
   <Switch>
-    <Route path="/trailpage" component={TrailPage} />
-    <Route path="/account" component={Account} />
-    <Route path="/trails/:id" component={TrailPage} />
-    <Route path="/search" component={Search} />
-    <Route exact path="/" component={Login} />
+    <Route path='/home' component={Home} />
+    <Route path='/trailpage' component={TrailPage} />
+    <Route path='/account' component={Account} />
+    <Route path='/trails/:id' component={TrailPage} />
+    <Route path='/search' component={Search} />
+    <Route exact path='/' component={Login} />
   </Switch>
 );
