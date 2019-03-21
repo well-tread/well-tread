f778efb04e95887347b27c6387c9a46f35eec6c7
Kevin Edmondson <kevinraye13@gmail.com>
2019-03-21 14:01:12 -0500

    top 5 biking trails homepage/ server call

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,29 +1,132 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import TrailResults from './TrailResults/TrailResults';
+import Typography from '@material-ui/core/Typography';
+import {
+  withStyles,
+  Theme,
+  createMuiTheme,
+  MuiThemeProvider
+} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#757575'
+    },
+    secondary: {
+      main: '#FF5722'
+    }
+  }
+});
 
 export interface Props {}
 
 export interface State {
   popularBikingTrails: any;
+  popularHikingTrails: any;
+  hikingArr: any;
+  bikingArr: any;
+  climbingArr: any;
+  isResultsBack: boolean;
+  latitude: number;
+  longitude: number;
 }
 
 class Home extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      popularBikingTrails: []
+      popularBikingTrails: [],
+      popularHikingTrails: [],
+      hikingArr: [],
+      bikingArr: [],
+      climbingArr: [],
+      isResultsBack: false,
+      latitude: 0,
+      longitude: 0
     };
   }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    const { hikingArr, bikingArr, climbingArr, isResultsBack } = this.state;
+    if (
+      !isResultsBack &&
+      (hikingArr.length > 0 || bikingArr.length > 0 || climbingArr.length > 0)
+    ) {
+      console.log('updating');
+      this.setState({ isResultsBack: true });
+    }
+  }
+  displayLocationInfo = (position: any) => {
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+    const lng = position.coords.longitude;
+    const lat = position.coords.latitude;
+    axios
+      .post('/trails/getPopularTrails', {
+        lat: lat,
+        lng: lng
+      })
+      //   .then(res => console.log('BIKE', res));
+      .then(res => {
+        const response = res.data;
+        //console.log(response);
+        this.setState({
+          popularBikingTrails: response
+        });
+      });
+
+    console.log(`longitude: ${lng} | latitude: ${lat}`);
+  };
   componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
+    }
+    // axios
+    //   .post('/trails/getPopularTrails', {
+    //     lat: this.state.latitude,
+    //     lng: this.state.longitude
+    //   })
+    //   .then(res => console.log('BIKE', res));
+    //   .then(res => {
+    //     const response = res.data;
+    //     console.log(response);
+    //     this.setState({
+    //       popularBikingTrails: response.trails
+    //     });
+    //   });
+
+    // axios
+    //   .get(
+    //     `https://www.mtbproject.com/data/get-trails?lat=${
+    //       this.state.latitude
+    //     }&lon=${
+    //       this.state.longitude
+    //     }&maxDistance=200&minStars=4&maxResults=5&key=200430946-fc66551e94fef44057cb0cc88316bbec`
+    //   )
+    //   .then(res => {
+    //     const response = res.data;
+    //     console.log(res.data.trails);
+    //     this.setState({
+    //       popularBikingTrails: response.trails
+    //     });
+    //   })
+
+    //   .catch(function(error) {
+    //     console.log(error);
+    //   });
     axios
       .get(
-        `https://www.mtbproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=200&minStars=4&maxResults=5&key=200430946-fc66551e94fef44057cb0cc88316bbec`
+        `https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=200&minStars=4&maxResults=5&key=200430946-fc66551e94fef44057cb0cc88316bbec`
       )
       .then(res => {
         const response = res.data;
-        console.log(res.data.trails);
+        //console.log(res.data.trails);
         this.setState({
-          popularBikingTrails: response.trails
+          popularHikingTrails: response.trails
         });
       })
 
@@ -33,8 +136,43 @@ class Home extends Component<Props, State> {
   }
 
   render() {
-    console.log(this.state.popularBikingTrails);
-    return <div>Best Home Page</div>;
+    const {
+      isResultsBack,
+      hikingArr,
+      bikingArr,
+      climbingArr,
+      popularBikingTrails,
+      popularHikingTrails
+    } = this.state;
+
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div>
+          <div>
+            <Typography variant='h5' color='primary' id='popular'>
+              Top 5 Biking Trails Near You
+            </Typography>
+          </div>
+          <TrailResults
+            popularBikingTrails={popularBikingTrails}
+            popularHikingTrails={[]}
+            hikingArr={[]}
+            bikingArr={[]}
+            climbingArr={[]}
+          />
+          <Typography variant='h5' color='primary' id='popular'>
+            Top 5 Hiking Trails Near You
+          </Typography>
+          <TrailResults
+            popularBikingTrails={[]}
+            hikingArr={[]}
+            popularHikingTrails={popularHikingTrails}
+            bikingArr={[]}
+            climbingArr={[]}
+          />
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
--- src/Components/Home/TrailResults/Result/Result.tsx
+++ src/Components/Home/TrailResults/Result/Result.tsx
@@ -1,30 +1,152 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-export interface Props{
+import { Redirect } from 'react-router-dom';
 
-}
+//materialUI imports
+import {
+  withStyles,
+  Theme,
+  createMuiTheme,
+  createStyles,
+  MuiThemeProvider
+} from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Button from '@material-ui/core/Button';
 
-export interface State{
-    
-}
-
-class Result extends Component<Props, State>{
-    constructor(props:Props){
-        super(props);
-        this.state={
-            
-        }
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#757575'
+    },
+    secondary: {
+      main: '#FF5722'
     }
-    
-    
+  }
+});
 
-    render(){
-        return(
-            <div>
-
-            </div>
-        )
+const styles = (theme: Theme) =>
+  createStyles({
+    typographyTitle: {
+      opacity: 1,
+      color: '#F7F7F7',
+      textShadow: '1px 1px 4px black',
+      fontSize: '1.3em'
+    },
+    typographyContent: {
+      opacity: 1,
+      color: '#F7F7F7',
+      textShadow: '1px 1px 4px black',
+      textAlign: 'left',
+      fontSize: '1.1em'
+    },
+    expansionPanel: {
+      backgroundColor: '#757575',
+      backgroundSize: `100% 100%`,
+      backgroundBlendMode: 'overlay',
+      paddingTop: 25,
+      paddingBottom: 25,
+      width: '100%'
+    },
+    expansionPanelExpanded: {
+      backgroundColor: '#757575',
+      backgroundSize: `100% 100%`,
+      backgroundBlendMode: 'overlay',
+      paddingTop: 25,
+      paddingBottom: 25,
+      margin: 0,
+      width: '100%'
+    },
+    expandMoreIcon: {
+      width: 30,
+      height: 30
     }
+  });
+
+export interface Props {
+  trail: any;
+  classes: {
+    typographyTitle: string;
+    typographyContent: string;
+    expansionPanel: string;
+    expansionPanelExpanded: string;
+    expandMoreIcon: string;
+  };
+  icon: any;
+  type: string;
 }
 
-export default Result;
+export interface State {
+  isRedirecting: boolean;
+}
+
+class Result extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isRedirecting: false
+    };
+  }
+
+  redirectToTrailPage() {
+    this.setState({ isRedirecting: true });
+  }
+
+  render() {
+    const { isRedirecting } = this.state;
+    const { trail, classes, icon, type } = this.props;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <ExpansionPanel
+          className={classes.expansionPanel}
+          style={{ backgroundImage: `url(${trail.imgMedium})` }}
+          classes={{ expanded: classes.expansionPanelExpanded }}
+        >
+          <ExpansionPanelSummary
+            expandIcon={
+              <ExpandMoreIcon
+                color='secondary'
+                className={classes.expandMoreIcon}
+              />
+            }
+          >
+            <Typography className={classes.typographyTitle}>
+              {icon} {trail.name}
+            </Typography>
+          </ExpansionPanelSummary>
+
+          <ExpansionPanelDetails>
+            <Typography className={classes.typographyContent}>
+              {trail.summary}
+            </Typography>
+          </ExpansionPanelDetails>
+
+          <ExpansionPanelActions>
+            <Button
+              color='secondary'
+              onClick={() => this.redirectToTrailPage()}
+              fullWidth
+            >
+              Trail Page
+            </Button>
+            <Button color='secondary' fullWidth>
+              Favorite
+            </Button>
+          </ExpansionPanelActions>
+        </ExpansionPanel>
+
+        {isRedirecting ? (
+          <Redirect to={`/trails/${type}/${trail.id}`} />
+        ) : (
+          <div />
+        )}
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default withStyles(styles)(Result);
